2931b29a9bcb6b22a864cb76c63195a14fd54d9e
chengcheng <chengchengm@isoftstone.com>
2021-02-18 14:41:50 +0800

    代码优化

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -2,21 +2,12 @@ package com.atguigu.gulimall.product;
 
 import com.alibaba.cloud.seata.feign.SeataFeignClientAutoConfiguration;
 import io.seata.config.springcloud.EnableSeataSpringConfig;
-import org.mybatis.spring.annotation.MapperScan;
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.redisson.spring.session.config.EnableRedissonHttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.io.IOException;
 
 @EnableSeataSpringConfig
 @EnableRedissonHttpSession
